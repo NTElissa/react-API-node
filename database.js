f49d94e12e631ba71108875bb982d97aff81c581
NTElissa <ntihindukaelissa77@gmail.com>
2024-10-28 18:22:00 +0200

    new update

--- database.js
+++ database.js
@@ -1,12 +1,13 @@
+// database.js
 import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/productdb');
-        console.log('MongoDB connected successfully');
+        await mongoose.connect(process.env.MongoDB);
+        console.log("MongoDB connected successfully");
     } catch (err) {
-        console.error('Failed to connect to MongoDB', err);
-        process.exit(1);  
+        console.error("Failed to connect to MongoDB", err);
+        process.exit(1);
     }
 };
 
--- server.js
+++ server.js
@@ -1,32 +1,17 @@
-import express from 'express';
-import connectDB from './database.js';
-import productRoutes from './Routes/productRoutes.js';
-import cors from 'cors';         
+// server.js
+import 'dotenv/config';  
+import express from "express";
+import connectDB from "./database.js"; 
+
+const app = express();
+const PORT = process.env.PORT ;
 
 connectDB();
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-// Middleware
-app.use(cors());                  
-app.use(express.json());           
-
-// Product Routes
-app.use('/products', productRoutes);
-
-// Root Route
-app.get('/', (req, res) => {
-  res.send('Welcome to the Product API');
+app.get("/", (req, res) => {
+    res.send("API is running...");
 });
 
-// Error Hndling Middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
-});
-
-// Start  server
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
